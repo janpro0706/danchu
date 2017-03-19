ee666536ab0da23bbc5dbb57ebd7c14eb015c142
jjeongg94 <lalala3423@hanmail.net>
2017-03-19 20:04:14 +0900

    Update review test

--- momuck-web/src/main/java/com/danchu/momuck/controller/ReviewController.java
+++ momuck-web/src/main/java/com/danchu/momuck/controller/ReviewController.java
@@ -2,11 +2,8 @@ package com.danchu.momuck.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,58 +14,57 @@ import com.danchu.momuck.service.ReviewService;
 import com.danchu.momuck.view.ResultView;
 import com.danchu.momuck.vo.Review;
 
-
 @Controller
 public class ReviewController {
-	
+
 	final private int USER_SEQ = 19;
-	
+
 	@Autowired
 	ReviewService reviewService;
-	
+
 	@ResponseBody
-    @RequestMapping(value = "/foods/{foods_id}/reviews", method = RequestMethod.POST, consumes = "application/json")
-    public ResultView submitReview(@PathVariable int foods_id,@RequestBody Review review) {
-    	 
-    	review.setUserSeq(USER_SEQ);
-    	review.setFoodSeq(foods_id);
-    	
-    	Review result = reviewService.submitReview(review);
-    	if(result == null){
-    		return new ResultView("500", "Server Internal Error", null);
-    	} 
-    	return new ResultView("200", "OK", result);
-    }
-    
+	@RequestMapping(value = "/foods/{foods_id}/reviews", method = RequestMethod.POST, consumes = "application/json")
+	public ResultView submitReview(@PathVariable int foods_id, @RequestBody Review review) {
+
+		review.setUserSeq(USER_SEQ);
+		review.setFoodSeq(foods_id);
+
+		Review result = reviewService.submitReview(review);
+		if (result == null) {
+			return new ResultView("500", "Server Internal Error", null);
+		}
+		return new ResultView("200", "OK", result);
+	}
+
 	@ResponseBody
-    @RequestMapping(value = "/foods/{foods_id}/reviews/{reviews_id}", method = RequestMethod.PUT, consumes = "application/json")
-    public ResultView updateReivew(@PathVariable int reviews_id, @RequestBody Review review) {
+	@RequestMapping(value = "/foods/{foods_id}/reviews/{reviews_id}", method = RequestMethod.PUT, consumes = "application/json")
+	public ResultView updateReivew(@PathVariable int reviews_id, @RequestBody Review review) {
 		review.setSeq(reviews_id);
-    	int result = reviewService.updateReview(review);
-    	if(result < 0){
-    		return new ResultView("500", "Server Internal Error", null);
-    	} 
-    	return new ResultView("200", "OK", null);
-    }
-    
+		int result = reviewService.updateReview(review);
+		if (result < 0) {
+			return new ResultView("500", "Server Internal Error", null);
+		}
+		return new ResultView("200", "OK", null);
+	}
+
 	@ResponseBody
-    @RequestMapping(value = "/foods/{foods_id}/reviews/{reviews_id}", method = RequestMethod.DELETE)
-    public ResultView deleteReivew(@PathVariable int reviews_id) {
-    	int result = reviewService.deleteReview(reviews_id);
-    	if(result < 0){
-    		return new ResultView("500", "Server Internal Error", null);
-    	} 
-    	return new ResultView("200", "OK", null);
-    }
-    
+	@RequestMapping(value = "/foods/{foods_id}/reviews/{reviews_id}", method = RequestMethod.DELETE)
+	public ResultView deleteReivew(@PathVariable int reviews_id) {
+		int result = reviewService.deleteReview(reviews_id);
+		if (result < 0) {
+			return new ResultView("500", "Server Internal Error", null);
+		}
+		return new ResultView("200", "OK", null);
+	}
+
 	@ResponseBody
-    @RequestMapping(value = "/foods/{foods_id}/reviews", method = RequestMethod.GET)
-    public ResultView getReviewList(@PathVariable int foods_id) {
-		
-    	List<Review> result = reviewService.selectReviewList(foods_id);
-    	if(result == null)
-    		return new ResultView("500", "No data", null);
-    	return new ResultView("200", "OK", result);
-        
-    }
+	@RequestMapping(value = "/foods/{foods_id}/reviews", method = RequestMethod.GET)
+	public ResultView getReviewList(@PathVariable int foods_id) {
+
+		List<Review> result = reviewService.selectReviewList(foods_id);
+		if (result == null)
+			return new ResultView("500", "No data", null);
+		return new ResultView("200", "OK", result);
+
+	}
 }
--- momuck-web/src/test/java/com/danchu/momuck/controller/ReviewControllerTest.java
+++ momuck-web/src/test/java/com/danchu/momuck/controller/ReviewControllerTest.java
@@ -1,7 +1,6 @@
 package com.danchu.momuck.controller;
 
 import static org.hamcrest.Matchers.hasKey;
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -24,26 +23,28 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.danchu.momuck.dao.ReviewDao;
+import com.danchu.momuck.vo.Review;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(locations = {"classpath:/root-context.xml", "classpath:servlet-context.xml"})
+@ContextConfiguration(locations = { "classpath:/root-context.xml", "classpath:servlet-context.xml" })
 public class ReviewControllerTest {
-	
-	Logger logger = Logger.getLogger(this.getClass());
 
 	private int foods_id = 1;
 	private int reviews_id;
-	
-	private float score;
-	private String message;
-	
-	@Autowired
-    private WebApplicationContext wac;
 
-    private MockMvc mockMvc;
-    
+	private static final float SCORE = (float) 5.0;
+	private static final String MESSAGE = "test";
+
+	String jsonParm = "{" + "\"score\" : \"" + SCORE + "\"," + "\"message\" : \"" + MESSAGE + "\"" + "}";
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mockMvc;
+
 	@Before
 	public void setUp() throws Exception {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
@@ -51,68 +52,57 @@ public class ReviewControllerTest {
 
 	@Test
 	public void submitReview() throws Exception {
-		
-		score = (float) 4.1;
-		message = "submit test";
-		
-		String jsonParm = "{" +
-	            "\"score\" : \"" + score + "\"," +
-	            "\"message\" : \"" + message + "\"" +              
-	            "}";
-		
-		this.mockMvc.perform(post("/foods/{foods_id}/reviews",foods_id)
-				.contentType(MediaType.APPLICATION_JSON).content(jsonParm))
-		.andExpect(status().isOk())
-        .andExpect(content().contentType("application/json;charset=UTF-8"))
-        .andExpect(jsonPath("$", hasKey("code")))
-        .andExpect(jsonPath("$.code").value("200"))
-        .andReturn();
+
+		MvcResult result = this.mockMvc
+				.perform(post("/foods/{foods_id}/reviews", foods_id).contentType(MediaType.APPLICATION_JSON)
+						.content(jsonParm))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$", hasKey("code")))
+				.andExpect(jsonPath("$.code").value("200"))
+				.andReturn();
+
+		String json = result.getResponse().getContentAsString();
+
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode node = mapper.readTree(json);
+		Review insertedReview = mapper.readValue(node.get("data").toString(), Review.class);
+
+		reviews_id = insertedReview.getSeq();
 	}
-	
+
 	@Test
 	public void updateReview() throws Exception {
-		
-		reviews_id = 1;
-		score = (float) 1.0;
-		message = "update test";
 
-		String jsonParm = "{" +
-	            "\"score\" : \"" + score + "\"," +
-	            "\"message\" : \"" + message + "\"" +              
-	            "}";
-		
-		this.mockMvc.perform(put("/foods/{foods_id}/reviews/{reviews_id}", foods_id, reviews_id)
-				.contentType(MediaType.APPLICATION_JSON).content(jsonParm))
-        .andExpect(status().isOk())
-        .andExpect(content().contentType("application/json;charset=UTF-8"))
-        .andExpect(jsonPath("$", hasKey("code")))
-        .andExpect(jsonPath("$.code").value("200"))
-        .andReturn();
+		this.mockMvc
+				.perform(put("/foods/{foods_id}/reviews/{reviews_id}", foods_id, reviews_id)
+						.contentType(MediaType.APPLICATION_JSON).content(jsonParm))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$", hasKey("code")))
+				.andExpect(jsonPath("$.code").value("200"))
+				.andReturn();
 	}
-	
+
 	@Test
 	public void deleteReview() throws Exception {
-		
-		reviews_id = 1;
-		
+
 		this.mockMvc.perform(delete("/foods/{foods_id}/reviews/{reviews_id}", foods_id, reviews_id))
-        .andExpect(status().isOk())
-        .andExpect(content().contentType("application/json;charset=UTF-8"))
-        .andExpect(jsonPath("$", hasKey("code")))
-        .andExpect(jsonPath("$.code").value("200"))
-        .andReturn();
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$", hasKey("code")))
+				.andExpect(jsonPath("$.code").value("200"))
+				.andReturn();
 	}
 
 	@Test
 	public void selectReview() throws Exception {
-		
-		MvcResult result = this.mockMvc.perform(get("/foods/{foods_id}/reviews", foods_id))
-        .andExpect(status().isOk())
-        .andExpect(content().contentType("application/json;charset=UTF-8"))
-        .andExpect(jsonPath("$", hasKey("code")))
-        .andExpect(jsonPath("$.code").value("200"))
-        .andReturn();
-		
-		logger.info(result.getResponse().getContentAsString());
+
+		this.mockMvc.perform(get("/foods/{foods_id}/reviews", foods_id)).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$", hasKey("code")))
+				.andExpect(jsonPath("$.code").value("200"))
+				.andReturn();
+
 	}
 }

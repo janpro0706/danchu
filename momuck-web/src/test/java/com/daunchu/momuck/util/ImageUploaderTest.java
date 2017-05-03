7499a3e099f3018aa9ed136afb40c6da82f4e972
geine <bonos2@wisdo.me>
2017-05-03 12:38:08 +0900

    Fix ImageUploaderTest

--- momuck-web/src/test/java/com/daunchu/momuck/util/ImageUploaderTest.java
+++ momuck-web/src/test/java/com/daunchu/momuck/util/ImageUploaderTest.java
@@ -1,5 +1,7 @@
 package com.daunchu.momuck.util;
 
+import com.danchu.momuck.util.MomuckImageUploader;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -12,7 +14,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.servlet.http.HttpServletRequest;
+
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -32,17 +37,22 @@ public class ImageUploaderTest {
 
 
     @Test
-    public void uploadAndDeleteImage() {
+    public void uploadAndDeleteImage() throws IOException {
+
         HttpServletRequest request = new MockMultipartHttpServletRequest();
+
         //여기 냄새남..ㅠㅠ
-        MockMultipartFile testImage = new MockMultipartFile("image","","application/json","{\"image\" : /TestImage.jpeg}".getBytes());
-        request.setAttribute("image", testImage);
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream fis = classloader.getResourceAsStream("static/TestImage.jpeg");
+        byte[] testImageBytes = IOUtils.toByteArray(fis);
+        MockMultipartFile testFile = new MockMultipartFile("image", "", "application/json", testImageBytes);
+
+        request.setAttribute("image", testFile);
+
         File image = momuckImageUploader.uploadImage(request);
         assertThat(image.canRead(), is(true));
+
         momuckImageUploader.deleteImage(image.getName());
         assertThat(image.exists(), is(false));
     }
-
-
-
 }

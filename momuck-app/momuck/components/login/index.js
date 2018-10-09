de8e55389336fd0986c4602423a3d843c8555269
Hyeondong Jang <janpro0706@gmail.com>
2018-10-09 12:24:41 +0900

    style login screen

--- momuck-app/momuck/components/login/index.js
+++ momuck-app/momuck/components/login/index.js
@@ -1,9 +1,11 @@
 import LoginForm from './LoginForm';
 import LoginTitle from './LoginTitle';
 import LoginView from './LoginView';
+import LoginButton from './LoginButton';
 
 export {
     LoginForm,
     LoginTitle,
-    LoginView
+    LoginView,
+    LoginButton
 };
